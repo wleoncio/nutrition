a74436e9f716b575266984399d686656b63747f1
Waldir Leoncio <w.l.netto@medisin.uio.no>
2022-12-28 18:07:18 +0100

    Added msj method for females

--- R/bmr.R
+++ R/bmr.R
@@ -7,15 +7,23 @@
 #' @param activity activity level (a scalar between 1 and 2)
 #' @param method calculation method ("msj" for Mifflin-St. Jeor or "kma" for
 #'  Katch-McArdle)
+#' @param gender "male" or "female"
 #' @return The Basal Metabolic Rate, in kilocalories
 #' @author Waldir Leoncio
 #' @references https://www.calculator.net/bmr-calculator.html
 #' @export
 #' @examples bmr(67, 40, .12, 178) # for an individual with 12% body fat
-bmr <- function(weight, age, fat, height, activity = 1.45, method = "msj") {
+bmr <- function(weight, age, fat, height, activity = 1.45, method = "msj", gender = "male") {
+  msj_constant <- switch(
+    gender,
+    "male" = 5,
+    "female" = -161,
+    stop("invalid gender for calculation")
+
+  )
   bmr <- switch(
     method,
-    "msj" = 9.99 * weight + 6.25 * height - 4.92 * age + 5,
+    "msj" = 9.99 * weight + 6.25 * height - 4.92 * age + msj_constant,
     "kma" = 370 + 21.6 * (1 - fat) * weight,
     stop("method must be either msj or kma")
   )
